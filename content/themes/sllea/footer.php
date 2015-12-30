1209792fd20754ad8af3c271333a852164ac2360
Mirabel Rice <rice122@mail.chapman.edu>
2015-12-29 22:04:47 -0800

    fixed privacy policy link

--- content/themes/sllea/footer.php
+++ content/themes/sllea/footer.php
@@ -1,7 +1,7 @@
 <?php
 	$logo_url = get_stylesheet_directory_uri() .'/images/mirbz.png';
 	$contact_url = get_bloginfo('url') . '/contact-us';
-	$privacy_url = get_bloginfo('url') . '/privacy-policy-2';
+	$privacy_url = get_bloginfo('url') . '/privacy-policy';
 	$terms_url = get_bloginfo('url') . '/terms';
 ?>
 
