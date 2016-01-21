ad359bec69ff4c25f1f6b4856c65462f7696b702
Mirabel Rice <rice122@mail.chapman.edu>
2016-01-20 20:46:41 -0800

    updated root dir for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :stage_domain, "dev.sllea.org"
 server "sllea@108.167.183.235", roles: %w{web app db}, port: 2222
-set :deploy_to, "/home3/sllea/public_html/dev"
+set :deploy_to, "/home3/sllea/dev"
 
 set :branch, "development"
